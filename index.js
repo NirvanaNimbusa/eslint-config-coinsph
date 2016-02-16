3f2a0ca625c84089bd8880b302161b59c5d73dec
Roman (Ky6uk) Nuritdinov <ky6uk.kun@gmail.com>
2016-02-17 01:45:15 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    // Disallow or Enforce Dangling Commas
+    'comma-dangle': [2, 'never'],
+
+    // Disallow Assignment in Conditional Statements
+    'no-cond-assign': [2, 'always'],
+
+    // Disallow Use of console
+    'no-console': 1,
+
+    // Disallow use of constant expressions in condition
+    'no-constant-condition': 2
+  }
+};
